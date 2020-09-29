526bcb7804681e6293bf492657058a75d5c4e7ce
Aleksi Simell <aleksi.simell@eficode.com>
2020-09-29 15:05:07 +0300

    Remove extra next() call

--- src/main/java/hudson/plugins/robot/RobotParser.java
+++ src/main/java/hudson/plugins/robot/RobotParser.java
@@ -303,7 +303,6 @@ public class RobotParser {
 									stackTrace.append("    " + reader.getText());
 									reader.next(); //skip text
 									reader.next(); //skip arg end
-									reader.next(); //skip WS
 								}
 							}
 							xmlTag = ignoreUntilStarts(reader, "kw", "status");
